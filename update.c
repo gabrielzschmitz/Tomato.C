d2ed0f2b4b188ec964e0acce5942667638b3590a
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-07 23:54:21 -0300

    📺 Rewriting the application with ncurses

---
+++ update.c
@@ -0,0 +1,103 @@
+/*
+//         .             .              .		    
+//         |             |              |           .	    
+// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,  
+// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /   
+// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'  
+//  ,|							    
+//  `'							    
+// util.c
+*/
+#include "util.h"
+#include "input.h"
+#include "update.h"
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+/* Update variables */
+void doUpdate(appData * app){
+
+    /*Mode 0*/
+    if(app->currentMode == 0){
+        frameTimer(app);
+
+        if(app->frameTimer == (1 * 16)) app->logoFrame = 1;
+        else if(app->frameTimer == (2 * 16)) app->logoFrame = 2;
+        else if(app->frameTimer == (3 * 16)) app->logoFrame = 3;
+        else if(app->frameTimer == (4 * 16)) app->logoFrame = 4;
+        else if(app->frameTimer == (5 * 16)) app->logoFrame = 5;
+        else if(app->frameTimer == (6 * 16)) app->logoFrame = 6;
+        else if(app->frameTimer == (7 * 16)) app->logoFrame = 7;
+        else if(app->frameTimer == (8 * 16)){
+            app->logoFrame = 0;
+            app->frameTimer = 0;
+        }
+    }
+
+    /*Mode 1*/
+    if(app->currentMode == 1){
+        timer(app);
+        frameTimer(app);
+        if(app->timer == 0){
+            if(app->pomodoroCounter == app->pomodoros){
+                app->timer = (app->longPause * 60 * 16);
+                app->pomodoroCounter = 0;
+                app->frameTimer = 0;
+                app->currentMode = 3;
+                system("notify-send -t 5000 -c cpomo ' Pause Break' 'You have some time to chill'");
+            }else{
+                app->timer = (app->shortPause * 60 * 16);
+                app->frameTimer = 0;
+                app->currentMode = 2;
+                system("notify-send -t 5000 -c cpomo ' Pause Break' 'You have some time chill'");
+            }
+        }
+
+        if(app->frameTimer == (3 * 16)) app->coffeeFrame = 1;
+        else if(app->frameTimer == (6 * 16)){
+            app->coffeeFrame = 0;
+            app->frameTimer = 0;
+        }
+    }
+
+    /*Mode 2*/
+    if(app->currentMode == 2){
+        timer(app);
+        frameTimer(app);
+        if(app->timer == 0){
+            app->timer = (app->workTime * 60 * 16);
+            app->frameTimer = 0;
+            app->currentMode = 1;
+            app->pomodoroCounter = app->pomodoroCounter + 1;
+            system("notify-send -t 5000 -c cpomo '華 Work!' 'You need to focus'");
+        }
+
+        if(app->frameTimer == (2 * 16)) app->machineFrame = 1;
+        else if(app->frameTimer == (4 * 16)) app->machineFrame = 2;
+        else if(app->frameTimer == (6 * 16)){
+            app->machineFrame = 0;
+            app->frameTimer = 0;
+        }
+    }
+
+    /*Mode 3*/
+    if(app->currentMode == 3){
+        timer(app);
+        frameTimer(app);
+        if(app->timer == 0)
+            app->currentMode = 0;
+
+        if(app->frameTimer == (3 * 16)) app->beachFrame = 1;
+        else if(app->frameTimer == (6 * 16)){
+            app->beachFrame = 0;
+            app->frameTimer = 0;
+        }
+    }
+
+    /*Get X and Y window size*/
+    getWindowSize(app);
+}
+
---
+++ update.h
@@ -0,0 +1,19 @@
+/*
+//         .             .              .		    
+//         |             |              |           .	    
+// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,  
+// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /   
+// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'  
+//  ,|							    
+//  `'							    
+// update.h
+*/
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+/* Update variables */
+extern void doUpdate(appData * );
+
