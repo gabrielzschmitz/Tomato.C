668270a7957649f87b7013c7e8f5a94cdb1a85fa
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-12 00:25:27 -0300

    🎞 Adding config.h options

--- anim.h
+++ anim.h
@@ -14,9 +14,9 @@
 extern void frameTimer(appData * );
 
 /* Printing the animations */
-extern void printLogo(appData * );
+extern void printLogo(appData * , const char * );
 extern void printCoffee(appData * );
-extern void printMachine(appData * );
+extern void printMachine(appData * , const char * );
 extern void printBeach(appData * );
 extern void printGear(appData * , int );
 
