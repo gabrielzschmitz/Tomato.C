4f558e8ec55b8d09117fc96a6cabf2942e9644fb
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-11 21:13:55 -0500

    ❄️  Added default.nix to build project on nix machines

---
+++ default.nix
@@ -0,0 +1,9 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "tomato";
+    src = ./.;
+    installPhase = "mkdir -p $out/bin && cp tomato $out/bin/";
+    buildInputs = with pkgs; [gcc gnumake ncurses];
+  }
