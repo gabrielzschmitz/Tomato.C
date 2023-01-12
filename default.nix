40b529a0d116d3b259617654eff66473532101fe
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-11 23:33:17 -0500

    ❄️  Added missing dependencies and metadata

--- default.nix
+++ default.nix
@@ -1,18 +1,43 @@
-#         .             .              .		  
-#         |             |              |           .	  
-# ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_, 
-# | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /  
-# `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"' 
-#  ,|							  
-#  `'							  
-# default.nix 
-
-let
+#         .             .              .
+#         |             |              |           .
+# ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,
+# | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /
+# `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'
+#  ,|
+#  `'
+# default.nix
+{}: let
   pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
 in
   pkgs.stdenv.mkDerivation {
     name = "tomato";
+
     src = ./.;
-    installPhase = "mkdir -p $out/bin && cp tomato $out/bin/";
-    buildInputs = with pkgs; [gcc gnumake ncurses];
+
+    installPhase = ''
+      mkdir -p $out/bin && cp tomato $out/bin/
+
+      ln -s $(which notify-send) $out/bin/
+      ln -s $(which mpv) $out/bin/
+    '';
+
+    buildInputs = with pkgs; [
+      gcc
+      which
+      gnumake
+      ncurses
+    ];
+
+    propagatedBuildInputs = with pkgs; [
+      libnotify
+      mpv
+    ];
+
+    meta = with lib; {
+      description = "A pomodoro timer written in pure C.";
+      homepage = "https://github.com/gabrielzschmitz/Tomato.C";
+      license = licenses.mit;
+      maintainers = with maintainers; [luisnquin];
+    };
   }
