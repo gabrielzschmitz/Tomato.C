2eb780c9c43aa58fc22e78d836005138b48857b7
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-15 11:18:23 -0300

    🔧 Adding flag -Wno-unused-result

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@
 PREFIX  = /usr/local
 APPPREFIX  = $(PREFIX)/share/applications
 CPPFLAGS = -I/usr/local/include
-CFLAGS  = -Wall -Wextra -pedantic -Os
+CFLAGS  = -Wall -Wextra -pedantic -Wno-unused-result -Os
 LDFLAGS = -L/usr/local/lib
 ifdef __APPLE__
 LDLIBS  = -lncurses
