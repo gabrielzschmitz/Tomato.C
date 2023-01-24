0869307b53435a6bc83a8f7ec15e136acdb58fc2
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-24 19:54:22 -0300

    🔧 Addding log file

--- config.mk
+++ config.mk
@@ -9,9 +9,12 @@
 
 PREFIX  = /usr/local
 APPPREFIX  = $(PREFIX)/share/applications
+LOGPREFIX=$(HOME)/.local/share/tomato
+
 CPPFLAGS = -I/usr/local/include
-CFLAGS  = -Wall -Wextra -pedantic -Wno-unused-result -Os
+CFLAGS  = -Wall -Wextra -pedantic -Wno-unused-result -Os -DLOGFILE=\"$(LOGPREFIX)/tomato.log\" -DTMPFILE=\"$(LOGPREFIX)/tmp.log\"
 LDFLAGS = -L/usr/local/lib
+
 ifdef __APPLE__
 LDLIBS  = -lncurses
 else
