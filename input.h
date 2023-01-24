06c06f09f9c206d445ffa9ea1cc3eca9f49776e0
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-24 19:55:38 -0300

    🕹 Addding log file

--- input.h
+++ input.h
@@ -13,6 +13,7 @@
 /* Defining some ASCII Keys */
 # define ESC 27
 # define ENTER 10
+# define CTRLC 3
 # define CTRLP 16
 # define CTRLR 18
 # define CTRLX 24
@@ -20,6 +21,7 @@
 /* Handle user input and app state */
 extern void handleInputs(appData * , const int , const int , const char * , const int );
 extern void mouseInput(appData * , MEVENT , char , const int , const int , const char * , const int );
+extern void resumeInput(appData * , char );
 extern void mainMenuInput(appData * , char , const int , const int , const char * , const int );
 extern void settingsInput(appData * , char );
 
