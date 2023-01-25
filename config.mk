4aacb9bd596804849d32fec3bd7934f1331112db
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-25 20:37:18 -0300

    🔧 Remove log file from Makefile

--- config.mk
+++ config.mk
@@ -9,10 +9,10 @@
 
 PREFIX  = /usr/local
 APPPREFIX  = $(PREFIX)/share/applications
-LOGPREFIX=$(HOME)/.local/share/tomato
+LOGPREFIX= .local/share/tomato
 
 CPPFLAGS = -I/usr/local/include
-CFLAGS  = -Wall -Wextra -pedantic -Wno-unused-result -Os -DLOGFILE=\"$(LOGPREFIX)/tomato.log\" -DTMPFILE=\"$(LOGPREFIX)/tmp.log\"
+CFLAGS  = -Wall -Wextra -pedantic -Wno-unused-result -Os -DLOGPREFIX=\"$(LOGPREFIX)\" -DLOGFILE=\"$(LOGPREFIX)/tomato.log\" -DTMPFILE=\"$(LOGPREFIX)/tmp.log\"
 LDFLAGS = -L/usr/local/lib
 
 ifdef __APPLE__
