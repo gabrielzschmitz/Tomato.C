0da6f0c652f330ba4bfd9ad6ba107795ebbe71ef
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-25 19:55:09 -0500

    ❄️  Fetching source from remote repository + pkgs import via ternary operator

--- default.nix
+++ default.nix
@@ -6,14 +6,17 @@
 #  ,|
 #  `'
 # default.nix
-{}: let
-  pkgs = import <nixpkgs> {};
-  lib = pkgs.lib;
+{pkgs ? import <nixpkgs> {}}: let
+  repository-url = "https://github.com/gabrielzschmitz/Tomato.C";
 in
   pkgs.stdenv.mkDerivation {
     name = "tomato";
 
-    src = ./.;
+    src = pkgs.fetchgit {
+      url = repository-url;
+      rev = "dda8e0ffe0b8612691ed336962f33580fb3a6038";
+      sha256 = "193xr63s4700bsamac0zjff0l6n8bcm60f546jfxqaafz126f9zv";
+    };
 
     installPhase = ''
       mkdir -p $out/bin && cp tomato $out/bin/
@@ -35,9 +38,9 @@ in
       mpv
     ];
 
-    meta = with lib; {
+    meta = with pkgs.lib; {
       description = "A pomodoro timer written in pure C.";
-      homepage = "https://github.com/gabrielzschmitz/Tomato.C";
+      homepage = repository-url;
       license = licenses.gpl3Plus;
       maintainers = with maintainers; [luisnquin];
     };
