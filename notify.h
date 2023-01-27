18232993f3d2a9fd758e8a060589a26dd93be8b3
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-01-27 18:36:47 -0300

    🔔 Refactoring app + adding end of cycle notification

---
+++ notify.h
@@ -0,0 +1,19 @@
+/*
+//         .             .              .		    
+//         |             |              |           .	    
+// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,  
+// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /   
+// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'  
+//  ,|							    
+//  `'							    
+// notify.h
+*/
+#ifndef NOTIFY_H_
+#define NOTIFY_H_
+#include <ncurses.h>
+
+/* Send a notification with sound */
+extern void notify(const char * );
+
+#endif
+
