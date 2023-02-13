54fef38881a5ee32cb0d346c3ea0ad54532ee216
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-02-13 13:22:20 -0300

    🔧 Adding white noise + mpv dependencie

--- config.mk
+++ config.mk
@@ -11,14 +11,14 @@ PREFIX  = /usr/local
 APPPREFIX  = $(PREFIX)/share/applications
 LOGPREFIX= .local/share/tomato
 
-DFLAGS = -DLOGPREFIX=\"$(LOGPREFIX)\" -DLOGFILE=\"$(LOGPREFIX)/tomato.log\" -DTMPFILE=\"$(LOGPREFIX)/tmp.log\" -DTIMERFILE=\"$(LOGPREFIX)/time.log\"
+DFLAGS = -D_POSIX_C_SOURCE -DTOMATONOISE=\"$(PREFIX)/bin/tomatonoise\" -DLOGPREFIX=\"$(LOGPREFIX)\" -DLOGFILE=\"$(LOGPREFIX)/tomato.log\" -DTMPFILE=\"$(LOGPREFIX)/tmp.log\" -DTIMERFILE=\"$(LOGPREFIX)/time.log\"
 CPPFLAGS = -I/usr/local/include
 CFLAGS  = -std=c99 -Wall -Wextra -pedantic -Wunused-result -Wno-unused-variable -Os ${DFLAGS}
 LDFLAGS = -L/usr/local/lib
 
 ifdef __APPLE__
-LDLIBS  = -lncurses
+LDLIBS  = -lncurses -lmpv
 else
-LDLIBS  = `pkg-config --libs ncursesw`
+LDLIBS  = `pkg-config --libs ncursesw mpv`
 endif
 
