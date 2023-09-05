5fa0d472ee474e5b69906b7d8fbf4e02103e0e51
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-09-05 20:23:49 -0300

    🔔 Fixing little code syntax inconsistency

--- notify.c
+++ notify.c
@@ -24,81 +24,77 @@
 #include <locale.h>
 
 /* Send a notification with sound */
-void notify(const char *message)
-{
-    if (strcmp(message, "autostartwork") == 0) /* Autostart work notification */{
-        if (strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
+void notify(const char *message){
+    /* Autostart work notification */
+    if(strcmp(message, "autostartwork") == 0){
+        if(strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
             send_notification("󱣲 A pomodoro has ended!", "Unpause to start the pause");
-        else if (strcmp(ICONS, "iconson") == 0 && WSL == 0)
+        else if(strcmp(ICONS, "iconson") == 0 && WSL == 0)
             send_notification("🔄 A pomodoro has ended!", "Unpause to start the pause");
         else
             send_notification("A pomodoro has ended!", "Unpause to start the pause");
 
         play_audio("/usr/local/share/tomato/sounds/dfltnotify.mp3");
     }
-    if (strcmp(message, "autostartpause") == 0) /* Autostart pause notification */{
-        if (strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
+    /* Autostart pause notification */
+    else if(strcmp(message, "autostartpause") == 0){ 
+        if(strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
             send_notification("󱣲 A pause has ended!", "Unpause to continue");
-        else if (strcmp(ICONS, "iconson") == 0 && WSL == 0)
+        else if(strcmp(ICONS, "iconson") == 0 && WSL == 0)
             send_notification("🔄 A pause has ended!", "Unpause to continue");
         else
             send_notification("A pause has ended!", "Unpause to continue");
 
         play_audio("/usr/local/share/tomato/sounds/pausenotify.mp3");
     }
-    else if (strcmp(message, "worktime") == 0) /* Work notification */
-    {
-        if (strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
+    /* Work notification */
+    else if(strcmp(message, "worktime") == 0){ 
+        if(strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
             send_notification("󱎫 Work!", "You need to focus");
-        else if (strcmp(ICONS, "iconson") == 0 && WSL == 0)
+        else if(strcmp(ICONS, "iconson") == 0 && WSL == 0)
             send_notification("👷 Work!", "You need to focus");
         else
             send_notification("Work!", "You need to focus!");
 
         play_audio("/usr/local/share/tomato/sounds/dfltnotify.mp3");
     }
-    else if (strcmp(message, "shortpause") == 0) /* Short Pause notification */
-    {
-        if (strcmp(ICONS, "nerdicons") == 0)
+    /* Short Pause notification */
+    else if(strcmp(message, "shortpause") == 0){
+        if(strcmp(ICONS, "nerdicons") == 0)
             send_notification(" Pause Break", "You have some time to chill");
-        else if (strcmp(ICONS, "iconson") == 0)
+        else if(strcmp(ICONS, "iconson") == 0)
             send_notification("☕ Pause Break", "You have some time to chill");
         else
             send_notification("Pause Break", "You have some time to chill");
 
         play_audio("/usr/local/share/tomato/sounds/pausenotify.mp3");
     }
-    else if (strcmp(message, "longpause") == 0) /* Long Pause notification */
-    {
-        if (strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
+    /* Long Pause notification */
+    else if(strcmp(message, "longpause") == 0){
+        if(strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
             send_notification(" Long Pause Break", "You have some time to chill");
-        else if (strcmp(ICONS, "iconson") == 0 && WSL == 0)
+        else if(strcmp(ICONS, "iconson") == 0 && WSL == 0)
             send_notification("🌴 Long Pause Break", "You have some time to chill");
         else
             send_notification("Long Pause Break", "You have some time to chill");
 
         play_audio("/usr/local/share/tomato/sounds/pausenotify.mp3");
     }
-    else if (strcmp(message, "end") == 0)/* End of cycle notification */
-    {
-
-        if (strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
+    /* End of cycle notification */
+    else{
+        if(strcmp(ICONS, "nerdicons") == 0 && WSL == 0)
             send_notification(" End of Pomodoro Cycle", "Feel free to start another!");
-        else if (strcmp(ICONS, "iconson") == 0 && WSL == 0)
+        else if(strcmp(ICONS, "iconson") == 0 && WSL == 0)
             send_notification("🍅 End of Pomodoro Cycle", "Feel free to start another!");
         else
             send_notification("End of Pomodoro Cycle", "Feel free to start another!");
 
         play_audio("/usr/local/share/tomato/sounds/endnotify.mp3");
     }
-    else{
-        return;
-    }
 }
 
-void send_notification(char *title, char *description)
-{
-    if (NOTIFY == 0)
+void send_notification(char *title, char *description){
+    if(NOTIFY == 0)
         return;
 
     int max_command_length = 256;
@@ -113,10 +109,8 @@ void send_notification(char *title, char *description)
     (void)system(command);
 }
 
-void play_audio(char *record_path)
-{
-    if (SOUND == 1 && WSL == 0)
-    {
+void play_audio(char *record_path){
+    if(SOUND == 1 && WSL == 0){
         int max_audio_cmd_length = 256;
 
         char *command[max_audio_cmd_length];
