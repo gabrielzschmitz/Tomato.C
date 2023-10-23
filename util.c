ec876412006c75cffa405b9a708fdd703a280dfa
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-10-23 15:51:02 -0300

    🌐 Adding notepad opening animation

--- util.c
+++ util.c
@@ -410,8 +410,8 @@ void timer(appData * app){
             if(app->timerms >= app->sfps){
                 app->timerms = 0;
                 /* Debug */
-                //app->timer = app->timer - 60;
-                app->timer = app->timer - 1;
+                app->timer = app->timer - 60;
+                //app->timer = app->timer - 1;
             }
         }
     }
