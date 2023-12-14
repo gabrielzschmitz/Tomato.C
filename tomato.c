febfc1f0199fd4c56b7facfb7d09930584600506
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2023-12-14 02:00:01 -0300

    🍅 Applying little optimizations

--- tomato.c
+++ tomato.c
@@ -1,272 +1,266 @@
 /*
-//         .             .              .		    
-//         |             |              |           .	    
-// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,  
-// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /   
-// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'  
-//  ,|							    
-//  `'							    
+//         .             .              .
+//         |             |              |           .
+// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,
+// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /
+// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'
+//  ,|
+//  `'
 //  tomato.c
 */
 #include "tomato.h"
 #include "anim.h"
+#include "config.h"
 #include "draw.h"
 #include "input.h"
 #include "notify.h"
 #include "update.h"
 #include "util.h"
-#include "config.h"
+#include <locale.h>
+#include <math.h>
 #include <ncurses.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include <locale.h>
 #include <unistd.h>
 
+#define FPS 60
+
 /* Initialize variables */
-void initApp(appData * app){
-    /* One Second Based in the Frames per Second */
-    app->sfps = sqrt(60);
+void initApp(appData *app) {
+  /* One Second Based in the Frames per Second */
+  app->sfps = sqrt(FPS);
 
-    /* Animation variables */
-    app->logoFrame = 0;
-    app->coffeeFrame= 0;
-    app->bannerFrame= 0;
-    app->helpFrame= 0;
-    app->notepadFrame= 0;
-    app->frameTimer = 0;
-    app->framems = 0;
+  /* Animation variables */
+  app->logoFrame = 0;
+  app->coffeeFrame = 0;
+  app->bannerFrame = 0;
+  app->helpFrame = 0;
+  app->notepadFrame = 0;
+  app->frameTimer = 0;
+  app->framems = 0;
 
-    /* Noise variables */
-    sprintf(app->rainVolume, "%d", RAINVOLUME);
-    sprintf(app->fireVolume, "%d", FIREVOLUME);
-    sprintf(app->windVolume, "%d", WINDVOLUME);
-    sprintf(app->thunderVolume, "%d", THUNDERVOLUME);
-    app->printVolume = 0;
-    app->rainNoisePID = 0;
-    app->fireNoisePID = 0;
-    app->windNoisePID = 0;
-    app->thunderNoisePID = 0;
-    app->runRainOnce = 0;
-    app->runFireOnce = 0;
-    app->runWindOnce = 0;
-    app->runThunderOnce = 0;
-    app->playNoise = 0;
-    app->playRainNoise = 0;
-    app->playFireNoise = 0;
-    app->playWindNoise = 0;
-    app->playThunderNoise = 0;
+  /* Noise variables */
+  sprintf(app->rainVolume, "%d", RAINVOLUME);
+  sprintf(app->fireVolume, "%d", FIREVOLUME);
+  sprintf(app->windVolume, "%d", WINDVOLUME);
+  sprintf(app->thunderVolume, "%d", THUNDERVOLUME);
+  app->printVolume = 0;
+  app->rainNoisePID = 0;
+  app->fireNoisePID = 0;
+  app->windNoisePID = 0;
+  app->thunderNoisePID = 0;
+  app->runRainOnce = 0;
+  app->runFireOnce = 0;
+  app->runWindOnce = 0;
+  app->runThunderOnce = 0;
+  app->playNoise = 0;
+  app->playRainNoise = 0;
+  app->playFireNoise = 0;
+  app->playWindNoise = 0;
+  app->playThunderNoise = 0;
 
-    /* Pomodoro variables */
-    app->pausedTimer = 0;
-    app->longPause = (LONGPAUSE * 60 * 8);
-    app->workTime = (WORKTIME * 60 * 8);
-    app->shortPause = (SHORTPAUSE * 60 * 8);
-    app->pomodoros = POMODOROS;
-    app->pomodoroCounter = 0;
-    app->cycles = 0;
-    app->newDay = 1;
-    app->autostartWork = AUTOSTARTWORK;
-    app->autostartPause = AUTOSTARTPAUSE;
+  /* Pomodoro variables */
+  app->pausedTimer = 0;
+  app->longPause = (LONGPAUSE * 60 * 8);
+  app->workTime = (WORKTIME * 60 * 8);
+  app->shortPause = (SHORTPAUSE * 60 * 8);
+  app->pomodoros = POMODOROS;
+  app->pomodoroCounter = 0;
+  app->cycles = 0;
+  app->newDay = 1;
+  app->autostartWork = AUTOSTARTWORK;
+  app->autostartPause = AUTOSTARTPAUSE;
 
-    /* Misc variables */
-    app->currentPID = getpid();
-    app->menuPos = 1;
-    app->helpLastMode = 0;
-    app->lastMode = 0;
-    app->currentMode = 0;
-    app->timer = 0;
-    app->timerms = 0;
-    app->needToLog = 0;
-    app->needResume = 0;
-    app->resume = 0;
-    app->runOnce = 1;
-    app->runHelpOnce = 1;
-    app->runNotepadOnce = 1;
+  /* Misc variables */
+  app->currentPID = getpid();
+  app->menuPos = 1;
+  app->helpLastMode = 0;
+  app->lastMode = 0;
+  app->currentMode = 0;
+  app->timer = 0;
+  app->timerms = 0;
+  app->needToLog = 0;
+  app->needResume = 0;
+  app->resume = 0;
+  app->runOnce = 1;
+  app->runHelpOnce = 1;
+  app->runNotepadOnce = 1;
 
-    /* Notepad variables */
-    createNotepad(&app->notes);
-    app->emptyNotepad = 1;
-    app->inputLength = 0;
-    app->notesAmount = 0;
-    app->inputMode = 'n';
-    app->editingNote = 0;
-    app->editingTask = 0;
-    app->addingNote = 0;
-    app->addingTask = 0;
-    app->currentNote = 0;
-    app->insertCursorx = 0;
-    app->insertCursory = 0;
-    app->cursorx = 0;
-    app->cursory = 0;
+  /* Notepad variables */
+  createNotepad(&app->notes);
+  app->emptyNotepad = 1;
+  app->inputLength = 0;
+  app->notesAmount = 0;
+  app->inputMode = 'n';
+  app->editingNote = 0;
+  app->editingTask = 0;
+  app->addingNote = 0;
+  app->addingTask = 0;
+  app->currentNote = 0;
+  app->insertCursorx = 0;
+  app->insertCursory = 0;
+  app->cursorx = 0;
+  app->cursory = 0;
 
-    /* File variables (defined in the config.mk) */
-    app->logPrefix = malloc(strlen(LOGPREFIX) + 1);
-    strcpy(app->logPrefix, LOGPREFIX);
-    if(WORKLOG == 1){
-        app->logFile = malloc(strlen(LOGFILE) + 1);
-        strcpy(app->logFile, LOGFILE);
-        app->tmpFile = malloc(strlen(TMPFILE) + 1);
-        strcpy(app->tmpFile, TMPFILE);
-    }
-    if(TIMERLOG == 1){
-        app->timerFile = malloc(strlen(TIMERFILE) + 1);
-        strcpy(app->timerFile, TIMERFILE);
-    }
-    if(NOTEPADLOG == 1){
-        app->notepadFile = malloc(strlen(NOTEPADFILE) + 1);
-        strcpy(app->notepadFile, NOTEPADFILE);
-    }
-    createLog(app);
-    readLog(app);
-    if(NOTEPADLOG == 1)
-        readNotepad(app);
+  /* File variables (defined in the config.mk) */
+  app->logPrefix = malloc(strlen(LOGPREFIX) + 1);
+  strcpy(app->logPrefix, LOGPREFIX);
+  if (WORKLOG == 1) {
+    app->logFile = malloc(strlen(LOGFILE) + 1);
+    strcpy(app->logFile, LOGFILE);
+    app->tmpFile = malloc(strlen(TMPFILE) + 1);
+    strcpy(app->tmpFile, TMPFILE);
+  }
+  if (TIMERLOG == 1) {
+    app->timerFile = malloc(strlen(TIMERFILE) + 1);
+    strcpy(app->timerFile, TIMERFILE);
+  }
+  if (NOTEPADLOG == 1) {
+    app->notepadFile = malloc(strlen(NOTEPADFILE) + 1);
+    strcpy(app->notepadFile, NOTEPADFILE);
+  }
+  createLog(app);
+  readLog(app);
+  if (NOTEPADLOG == 1) readNotepad(app);
 }
 
 /* Update variables */
-void doUpdate(appData * app){
-    /* Update all the app modes */
-    if(app->currentMode == -3)
-        updateHelpPage(app);
-    else if(app->currentMode == -2)
-        updateNotepad(app);
-    else if(app->currentMode == 0)
-        updateMainMenu(app);
-    else if(app->currentMode == 1)
-        updateWorkTime(app);
-    else if(app->currentMode == 2)
-        updateShortPause(app);
-    else if(app->currentMode == 3)
-        updateLongPause(app);
+void doUpdate(appData *app) {
+  /* Update all the app modes */
+  if (app->currentMode == -3)
+    updateHelpPage(app);
+  else if (app->currentMode == -2)
+    updateNotepad(app);
+  else if (app->currentMode == 0)
+    updateMainMenu(app);
+  else if (app->currentMode == 1)
+    updateWorkTime(app);
+  else if (app->currentMode == 2)
+    updateShortPause(app);
+  else if (app->currentMode == 3)
+    updateLongPause(app);
 
-    /* Get X and Y window size */
-    getWindowSize(app);
+  /* Get X and Y window size */
+  getWindowSize(app);
 }
 
 /* Print at screen */
-void drawScreen(appData * app){
-    erase();
-    
-    switch(app->currentMode){
-        case -3:
-            printPergament(app);
-            printNotepadIndicator(app);
-            printHelpIndicator(app);
-            printNoiseMenu(app);
-            break;
-        case -2:
-            printNotepad(app);
-            printNotepadIndicator(app);
-            printHelpIndicator(app);
-            printNoiseMenu(app);
-            if(app->inputMode == 'n' && app->notepadFrame == 6)
-                printCursor(app);
-            break;
+void drawScreen(appData *app) {
+  erase();
 
-        case -1:
-            printWrench(app, 1);
-            printSettings(app);
-            printWrench(app, 0);
-            printNoiseMenu(app);
-            printHelpIndicator(app);
-            if(NOTEPAD == 1)
-                printNotepadIndicator(app);
-            break;
+  switch (app->currentMode) {
+    case -3:
+      printPergament(app);
+      printNotepadIndicator(app);
+      printHelpIndicator(app);
+      printNoiseMenu(app);
+      break;
+    case -2:
+      printTimer(app);
+      printNotepad(app);
+      printNotepadIndicator(app);
+      printHelpIndicator(app);
+      printNoiseMenu(app);
+      if (app->inputMode == 'n' && app->notepadFrame == 6) printCursor(app);
+      break;
 
-        case 0:
-            printResume(app);
-            printMainMenu(app);
-            printNoiseMenu(app);
-            if(NOTEPAD == 1 && app->needResume != 1){
-                printNotepadIndicator(app);
-                printHelpIndicator(app);
-            }
-            break;
+    case -1:
+      printWrench(app, 1);
+      printSettings(app);
+      printWrench(app, 0);
+      printNoiseMenu(app);
+      printHelpIndicator(app);
+      if (NOTEPAD == 1) printNotepadIndicator(app);
+      break;
 
-        case 1:
-            printPomodoroCounter(app);
-            printPauseIndicator(app);
-            printCoffee(app);
-            printTimer(app);
-            printNoiseMenu(app);
-            printHelpIndicator(app);
-            if(NOTEPAD == 1)
-                printNotepadIndicator(app);
-            break;
+    case 0:
+      printResume(app);
+      printMainMenu(app);
+      printNoiseMenu(app);
+      if (NOTEPAD == 1 && app->needResume != 1) {
+        printNotepadIndicator(app);
+        printHelpIndicator(app);
+      }
+      break;
 
-        case 2:
-            printPomodoroCounter(app);
-            printPauseIndicator(app);
-            printMachine(app);
-            printTimer(app);
-            printNoiseMenu(app);
-            printHelpIndicator(app);
-            if(NOTEPAD == 1)
-                printNotepadIndicator(app);
-            break;
+    case 1:
+      printPomodoroCounter(app);
+      printPauseIndicator(app);
+      printCoffee(app);
+      printTimer(app);
+      printNoiseMenu(app);
+      printHelpIndicator(app);
+      if (NOTEPAD == 1) printNotepadIndicator(app);
+      break;
 
-        case 3:
-            printPomodoroCounter(app);
-            printPauseIndicator(app);
-            printBeach(app);
-            printTimer(app);
-            printNoiseMenu(app);
-            printHelpIndicator(app);
-            if(NOTEPAD == 1)
-                printNotepadIndicator(app);
-            break;
-        default:
-            break;
-    }
-    refresh();
+    case 2:
+      printPomodoroCounter(app);
+      printPauseIndicator(app);
+      printMachine(app);
+      printTimer(app);
+      printNoiseMenu(app);
+      printHelpIndicator(app);
+      if (NOTEPAD == 1) printNotepadIndicator(app);
+      break;
+
+    case 3:
+      printPomodoroCounter(app);
+      printPauseIndicator(app);
+      printBeach(app);
+      printTimer(app);
+      printNoiseMenu(app);
+      printHelpIndicator(app);
+      if (NOTEPAD == 1) printNotepadIndicator(app);
+      break;
+    default:
+      break;
+  }
+  refresh();
 }
 
 /* Putting it all together */
-int main(int argc, char *argv[]){
-    if(argc == 2 && !strcmp("-t", argv[1]) && TIMERLOG == 1){
-        char * timerFile = malloc(strlen(TIMERFILE) + 1);
-        strcpy(timerFile, TIMERFILE);
-        int status = tomatoTimer(timerFile);
-        return status;
-    }
-    else if(argc == 2 && !strcmp("-t", argv[1]) && TIMERLOG != 1){
-        printf("enable timer log to use [-t]");
-        return 1;
-    }
-    else if(argc != 1){
-        printf("usage: tomato [-t]");
-        return 0;
-    }
-    /* Enable Emojis */
-    setlocale(LC_CTYPE, "");
-
-    /* Creating the app struct */
-    appData app;
-
-    /* Initializing the app */
-    initScreen();
-    initApp(&app);
-
-    /* Main app loop */
-    while(1){
-        handleInputs(&app);
-
-        doUpdate(&app);
-
-        drawScreen(&app);
-
-        /* Setting the screen refresh rate to 60 */
-        napms(1000 / 60);
-    }
-
-    /* Makes terminal stop reporting mouse movement events */
-    printf("\033[?1003l\n");
-
-    /* Endding the screen created at initScreen */
-    endwin();
-
+int main(int argc, char *argv[]) {
+  if (argc == 2 && !strcmp("-t", argv[1]) && TIMERLOG == 1) {
+    char *timerFile = malloc(strlen(TIMERFILE) + 1);
+    strcpy(timerFile, TIMERFILE);
+    int status = tomatoTimer(timerFile);
+    return status;
+  } else if (argc == 2 && !strcmp("-t", argv[1]) && TIMERLOG != 1) {
+    printf("enable timer log to use [-t]");
+    return 1;
+  } else if (argc != 1) {
+    printf("usage: tomato [-t]");
     return 0;
-}
+  }
+  /* Enable Emojis */
+  setlocale(LC_CTYPE, "");
 
+  /* Creating the app struct */
+  appData app;
+
+  /* Initializing the app */
+  initScreen();
+  initApp(&app);
+
+  /* Main app loop */
+  while (1) {
+    handleInputs(&app);
+
+    doUpdate(&app);
+
+    drawScreen(&app);
+
+    /* Setting the screen refresh rate to 60 */
+    napms(1000 / FPS);
+  }
+
+  /* Makes terminal stop reporting mouse movement events */
+  printf("\033[?1003l\n");
+
+  /* Endding the screen created at initScreen */
+  endwin();
+
+  return 0;
+}
--- tomato.h
+++ tomato.h
@@ -1,11 +1,11 @@
 /*
-//         .             .              .		    
-//         |             |              |           .	    
-// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,  
-// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /   
-// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'  
-//  ,|							    
-//  `'							    
+//         .             .              .
+//         |             |              |           .
+// ,-. ,-. |-. ,-. . ,-. |  ,_, ,-. ,-. |-. ,-,-. . |- ,_,
+// | | ,-| | | |   | |-' |   /  `-. |   | | | | | | |   /
+// `-| `-^ ^-' '   ' `-' `' '"' `-' `-' ' ' ' ' ' ' `' '"'
+//  ,|
+//  `'
 // tomato.h
 */
 #ifndef TOMATO_H_
@@ -21,124 +21,123 @@
 
 // Defining the notes struct
 typedef struct note note;
-struct note{
-    char *note;
-    char type;
+struct note {
+  char *note;
+  char type;
 };
 
 // Defining the Notepad
 typedef struct notepad notepad;
-struct notepad{
-    note *lines[MAXLINES];
+struct notepad {
+  note *lines[MAXLINES];
 };
 
 /* Defining the app struct */
 typedef struct appData appData;
-struct appData{
-    int insertCursory;
-    int insertCursorx;
-    int cursory;
-    int cursorx;
-    notepad notes;
-    int currentNote;
-    int emptyNotepad;
-    int notesAmount;
-    int inputLength;
-    char inputMode;
-    int editingNote;
-    int editingTask;
-    int addingNote;
-    int addingTask;
-    int currentPID;
-    int rainNoisePID;
-    int fireNoisePID;
-    int windNoisePID;
-    int thunderNoisePID;
-    int runRainOnce;
-    int runFireOnce;
-    int runWindOnce;
-    int runThunderOnce;
-    int playNoise;
-    int playRainNoise;
-    int playFireNoise;
-    int playWindNoise;
-    int playThunderNoise;
-    int printVolume;
-    int printRainVolume;
-    int printFireVolume;
-    int printWindVolume;
-    int printThunderVolume;
-    char rainVolume[4];
-    char fireVolume[4];
-    char windVolume[4];
-    char thunderVolume[4];
-    char * mpvCmd;
-    char * logPrefix;
-    char * logFile;
-    char * tmpFile;
-    char * timerFile;
-    char * notepadFile;
-    int pomodorosLevels;
-    int workTimeLevels;
-    int shortPauseLevels;
-    int longPauseLevels;
-    int pomodoros;
-    int workTime;
-    int shortPause;
-    int longPause;
-    int menuPos;
-    int pomodoroCounter;
-    int lastMode;
-    int helpLastMode;
-    int runHelpOnce;
-    int runNotepadOnce;
-    int currentMode;
-    int needMainMenu;
-    int frameTimer;
-    int timer;
-    double sfps;
-    int framems;
-    int timerms;
-    int logoFrame;
-    int coffeeFrame;
-    int machineFrame;
-    int beachFrame;
-    int bannerFrame;
-    int helpFrame;
-    int notepadFrame;
-    int userInput;
-    int lastInput;
-    int pausedTimer;
-    int cycles;
-    int needToLog;
-    int autostartWork; 
-    int autostartPause; 
-    int unfinishedPomodoroCounter;
-    int unfinishedPomodoros;
-    int unfinishedTimer;
-    int unfinishedFullTimer;
-    int needResume;
-    int resume;
-    int newDay;
-    int runOnce; 
-    char date[50];
-    int middlex;
-    int middley;
-    int x;
-    int y;
+struct appData {
+  int insertCursory;
+  int insertCursorx;
+  int cursory;
+  int cursorx;
+  notepad notes;
+  int currentNote;
+  int emptyNotepad;
+  int notesAmount;
+  int inputLength;
+  char inputMode;
+  int editingNote;
+  int editingTask;
+  int addingNote;
+  int addingTask;
+  int currentPID;
+  int rainNoisePID;
+  int fireNoisePID;
+  int windNoisePID;
+  int thunderNoisePID;
+  int runRainOnce;
+  int runFireOnce;
+  int runWindOnce;
+  int runThunderOnce;
+  int playNoise;
+  int playRainNoise;
+  int playFireNoise;
+  int playWindNoise;
+  int playThunderNoise;
+  int printVolume;
+  int printRainVolume;
+  int printFireVolume;
+  int printWindVolume;
+  int printThunderVolume;
+  char rainVolume[4];
+  char fireVolume[4];
+  char windVolume[4];
+  char thunderVolume[4];
+  char *mpvCmd;
+  char *logPrefix;
+  char *logFile;
+  char *tmpFile;
+  char *timerFile;
+  char *notepadFile;
+  int pomodorosLevels;
+  int workTimeLevels;
+  int shortPauseLevels;
+  int longPauseLevels;
+  int pomodoros;
+  int workTime;
+  int shortPause;
+  int longPause;
+  int menuPos;
+  int pomodoroCounter;
+  int lastMode;
+  int helpLastMode;
+  int runHelpOnce;
+  int runNotepadOnce;
+  int currentMode;
+  int needMainMenu;
+  int frameTimer;
+  int timer;
+  double sfps;
+  int framems;
+  int timerms;
+  int logoFrame;
+  int coffeeFrame;
+  int machineFrame;
+  int beachFrame;
+  int bannerFrame;
+  int helpFrame;
+  int notepadFrame;
+  int userInput;
+  int lastInput;
+  int pausedTimer;
+  int cycles;
+  int needToLog;
+  int autostartWork;
+  int autostartPause;
+  int unfinishedPomodoroCounter;
+  int unfinishedPomodoros;
+  int unfinishedTimer;
+  int unfinishedFullTimer;
+  int needResume;
+  int resume;
+  int newDay;
+  int runOnce;
+  char date[50];
+  int middlex;
+  int middley;
+  int x;
+  int y;
 };
 
 /* Initialize variables */
-extern void initApp(appData * );
+extern void initApp(appData *);
 
 /* Update variables */
-extern void doUpdate(appData * );
+extern void doUpdate(appData *);
 
 /* Print at screen */
-extern void drawScreen(appData * );
+extern void drawScreen(appData *);
 
 /* Putting it all together */
-extern int main(int , char *[]);
+extern int main(int, char *[]);
 
 #endif
-
