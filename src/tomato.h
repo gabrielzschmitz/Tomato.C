df8d1e14efbb6e34aea51a79da11f8dd7fa58f94
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2024-12-31 02:43:49 -0300

    style(tomato.h): standardize formatting and enhance AppData structure
    - Adjusted macro definitions for consistent spacing and alignment.
    - Standardized pointer declarations to use `Type* name` style.
    - Added a `last_input` field to the `AppData` structure to support
      improved input handling logic.
    
    These changes enhance code readability, maintainability, and align with
    project-wide formatting conventions.

--- src/tomato.h
+++ src/tomato.h
@@ -8,12 +8,12 @@
 #include "bar.h"
 #include "config.h"
 
-#define PALETTE_SIZE    (COLOR_WHITE - COLOR_BLACK + 1)
-#define NO_COLOR        -1
-#define MAX_ANIMATIONS  7
-#define MAX_MENUS       2
+#define PALETTE_SIZE (COLOR_WHITE - COLOR_BLACK + 1)
+#define NO_COLOR -1
+#define MAX_ANIMATIONS 7
+#define MAX_MENUS 2
 #define MAX_INPUT_MODES 3
-#define FPMS            1000.0 / FPS
+#define FPMS 1000.0 / FPS
 
 #ifdef DEBUG_FLAG
 #define DEBUG 1
@@ -53,14 +53,15 @@ typedef enum {
 
 /* Defining the app struct */
 struct AppData {
-  struct Screen *screen;
-  struct StatusBar *status_bar;
-  struct Menu *menus[MAX_MENUS];
+  struct Screen* screen;
+  struct StatusBar* status_bar;
+  struct Menu* menus[MAX_MENUS];
 
   int user_input;
+  int last_input;
   bool block_input;
 
-  struct Rollfilm *animations[MAX_ANIMATIONS];
+  struct Rollfilm* animations[MAX_ANIMATIONS];
   bool is_paused;
   bool running;
 };
