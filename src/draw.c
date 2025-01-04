5c0e02aca9645530385ab6dcae4d41f2e7e0e5c5
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2025-01-04 00:58:12 -0300

    style(draw): adjust formatting of debug output for readability
    - Refactor the conditional block in `DrawScreen` for cleaner formatting.
    - Align the debug output to improve code readability without altering
      functionality.
    
    This change ensures consistent code style and improves maintainability.

--- src/draw.c
+++ src/draw.c
@@ -57,9 +57,9 @@ ErrorType DrawScreen(AppData* app) {
         break;
       default: break;
     }
-    if (DEBUG && current_panel->visible){
-      mvprintw(1, 2, "%c/%c - %dcm", app->user_input, app->last_input, app->current_menu);
-    }
+    if (DEBUG && current_panel->visible)
+      mvprintw(1, 2, "%c/%c - %dcm", app->user_input, app->last_input,
+          app->current_menu);
 
     SetColor((app->screen->current_panel == i) ? FOCUSED_PANEL_COLOR
                                                : UNFOCUSED_PANEL_COLOR,
